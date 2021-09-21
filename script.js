2daf927a3a8c26404db598eef87882e13456e0e9
Kotamora <velvarion@gmail.com>
2021-09-21 23:33:05 +0300

    test

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let page = document.querySelector('body');
+let page = document.querySelector('.page');
 let themeButton = document.querySelector('.theme-button');
-themeButton.onclick = function () {
-  page.classList.toggle('dark-theme');
+themeButton.onclick = function(){
+    page.classList.toggle('dark-theme');
 };
